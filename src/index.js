f03431c0c668e0a751723416f5f70786210897f4
qili <chao0099xx@gmail.com>
2024-10-04 15:03:04 +0800

    回退项目至清洁状态

--- src/index.js
+++ src/index.js
@@ -1,37 +1,11 @@
 import _ from "lodash";
-import "./style.css";
-import pic from './demo_pic.png';
-import data from './data.csv';
-import notes from './data.xml';
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // 输出 `TOML Example`
-console.log(toml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(yaml.title); // 输出 `YAML Example`
-console.log(yaml.owner.name); // 输出 `Tom Preston-Werner`
-
-console.log(json.title); // 输出 `JSON5 Example`
-console.log(json.owner.name); // 输出 `Tom Preston-Werner
 
 function component() {
   const element = document.createElement("div");
 
   // lodash 从 npm 包中导入 join 方法
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-  element.classList.add("hello");
 
-  const img = new Image();
-  img.src = pic;
-  img.width = 200;
-  img.height = 200;
-  element.appendChild(img);
-
-  console.log('csv data',data);
-  console.log('xml notes',notes);
-  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,4 @@
 const path = require("path");
-const toml = require("toml");
-const yaml = require("yamljs");
-const json5 = require("json5");
 
 module.exports = {
   mode: "production",
@@ -9,17 +6,5 @@ module.exports = {
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
-  },
-  module: {
-    rules: [
-      { test: /\.css$/i, use: ["style-loader", "css-loader"] }, // use style-loader and css-loader to load css files
-      { test: /\.xml/i, use: ["xml-loader"] }, // use xml-loader to load xml files
-      { test: /\.(csv|tsv)$/i, use: ["csv-loader"] }, // use csv-loader to load csv and tsv files
-      { test: /\.(png|jpe?g|gif|svg)$/i, type: "asset/resource" }, //使用webpack内置的asset/resource模块来处理图片资源
-      { test: /\.(woff|woff2|eot|ttf|otf)$/i, type: "asset/resource" }, //使用webpack内置的asset/resource模块来处理字体资源
-      { test: /\.toml$/i, type: "json", parser: { parse: toml.parse } }, // 使用toml来解析toml文件
-      { test: /\.(yaml|yml)$/i, type: "json", parser: { parse: yaml.parse } }, // 使用yamljs来解析yaml文件
-      { test: /\.json5$/i, type: "json", parser: { parse: json5.parse } }, // 使用json5来解析json5文件
-    ],
-  },
+  }
 };
