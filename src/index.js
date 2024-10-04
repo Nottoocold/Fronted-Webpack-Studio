830d7ebc85ade6bcab36d1fe5be3e88380c765aa
qili <chao0099xx@gmail.com>
2024-10-04 14:57:27 +0800

    使用webpack配置自定义json模块解析

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import "./style.css";
 import pic from './demo_pic.png';
 import data from './data.csv';
 import notes from './data.xml';
+import toml from './data.toml';
+import yaml from './data.yaml';
+import json from './data.json5';
+
+console.log(toml.title); // 输出 `TOML Example`
+console.log(toml.owner.name); // 输出 `Tom Preston-Werner`
+
+console.log(yaml.title); // 输出 `YAML Example`
+console.log(yaml.owner.name); // 输出 `Tom Preston-Werner`
+
+console.log(json.title); // 输出 `JSON5 Example`
+console.log(json.owner.name); // 输出 `Tom Preston-Werner
 
 function component() {
   const element = document.createElement("div");
