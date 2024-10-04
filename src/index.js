5989c61edc2da7053576fa2bf12c2d252a9afbae
qili <chao0099xx@gmail.com>
2024-10-04 15:22:58 +0800

    使用webpack配置多个入口文件并管理输出结果

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import _ from "lodash";
+import printMe from "./print.js";
 
 function component() {
   const element = document.createElement("div");
-
+  const btn = document.createElement("button");
+  btn.innerHTML = "Click me and check the console!";
+  btn.onclick = printMe;
+  
   // lodash 从 npm 包中导入 join 方法
   element.innerHTML = _.join(["Hello", "webpack"], " ");
-
+  
+  element.appendChild(btn);
+  
+  
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,12 @@ const path = require("path");
 
 module.exports = {
   mode: "production",
-  entry: ["./src/index.js"],
+  entry: {
+    index: "./src/index.js", // 入口文件1
+    print: "./src/print.js", // 入口文件2
+  },
   output: {
-    filename: "bundle.js",
+    filename: "[name].bundle.js", // 根据入口文件名生成打包后的文件名
     path: path.resolve(__dirname, "dist"),
   }
 };
