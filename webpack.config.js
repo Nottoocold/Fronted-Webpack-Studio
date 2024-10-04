8badb26d97a4260d231eac14cfce7b1b9c974c29
qili <chao0099xx@gmail.com>
2024-10-04 20:17:54 +0800

    打包字体媒体等资源

--- webpack.config.js
+++ webpack.config.js
@@ -21,9 +21,14 @@ module.exports = {
       { test: /\.css/i, use: ["style-loader", "css-loader"] },
       {
         test: /\.(png|jpe?g|gif|svg|webp)$/i,
-        type: "asset",
-        generator: { filename: "img/[hash][ext][query]" },
+        type: "asset", // 处理图片资源, 对于小于4kb的图片会内联到js文件中，大于4kb的图片会单独打包成文件
+        generator: { filename: "img/[hash:10][ext][query]" },
         parser: { dataUrlCondition: { maxSize: 4 * 1024 } },
+      },
+      {
+        test: /\.(ttf|woff2?|mp3|mp4|avi)$/i,
+        type: "asset/resource", // 处理其他资源 如字体文件
+        generator: { filename: "img/[hash:10][ext][query]" },
       },
     ],
   },
