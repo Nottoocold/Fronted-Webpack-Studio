e14c1cf801c8907915f44378b2e2f23b3ba71366
qili <chao0099xx@gmail.com>
2024-10-04 15:39:25 +0800

    webpack 开发环境source map功能

--- src/print.js
+++ src/print.js
@@ -1,3 +1,4 @@
 export default function printMe() {
     console.log('I get called from print.js!');
+    // conssole.log('I get called from print.js!'); // 测试开发环境webpack的source map功能
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,13 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "production",
+  mode: "development",
   entry: {
     index: "./src/index.js", // 入口文件1
     print: "./src/print.js", // 入口文件2
   },
-  plugins: [new HtmlWebpackPlugin({title: '管理输出'}),], // 自动生成html文件
+  devtool: 'inline-source-map', // 开启source-map 仅在开发环境下生效，生产环境下需要注释掉
+  plugins: [new HtmlWebpackPlugin({title: '开发环境'}),], // 自动生成html文件
   output: {
     filename: "[name].bundle.js", // 根据入口文件名生成打包后的文件名
     path: path.resolve(__dirname, "dist"),
