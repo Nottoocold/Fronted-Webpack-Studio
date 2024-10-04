779f7876f979c357e39bbcab95b70b64ad325a18
qili <chao0099xx@gmail.com>
2024-10-04 15:33:50 +0800

    使用webpack动态生成html，并倒入js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -6,8 +7,10 @@ module.exports = {
     index: "./src/index.js", // 入口文件1
     print: "./src/print.js", // 入口文件2
   },
+  plugins: [new HtmlWebpackPlugin({title: '管理输出'}),], // 自动生成html文件
   output: {
     filename: "[name].bundle.js", // 根据入口文件名生成打包后的文件名
     path: path.resolve(__dirname, "dist"),
-  }
+    clean: true, // 每次打包前清空dist目录
+  },
 };
