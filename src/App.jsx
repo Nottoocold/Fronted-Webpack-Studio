4ced66e1d298be7b83f91946a5ba2e12738a125e
qili <chaoqili09@gitm.com>
2024-10-14 23:48:31 +0800

    最简单的react-router-dom例子

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import Home from "@/pages/Home";
 import "./App.less"; // 全局样式
-import { ConfigProvider } from "antd";
+import { createBrowserRouter, RouterProvider, Router, Link } from "react-router-dom";
 
-export default function App() {
-  return (
-    <ConfigProvider
-      theme={{
-        token: {
-          // 更多详情，参考https://ant-design.antgroup.com/docs/react/customize-theme-cn
-          // Seed Token，影响范围大
-          colorPrimary: "#00b96b",
-          
-          // 派生变量，影响范围小
-          colorBgContainer: "#f6ffed",
-        },
-      }}
-    >
-      <h1>Welcome to Fronted Webpack Studio</h1>
-      <Home />
-    </ConfigProvider>
-  );
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <div>this is login page.</div> },
+]);
+
+
+function App() {
+  return <RouterProvider router={router} />;
 }
+
+export default App;
