ee80b525ae74c70ca6650ecd8135845db8ad5632
qili <chaoqili09@gitm.com>
2024-10-14 22:51:59 +0800

    添加react-router-dom依赖

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
-import "./index.less";
-import { createRoot } from 'react-dom/client'; // react 18+ version of createRoot, else react-dom
+import { createRoot } from "react-dom/client"; // react 18+ version of createRoot, else react-dom
 import App from "./App";
+import React, { StrictMode } from "react";
 
-const root = createRoot(document.getElementById("root"));
-
-root.render(<App />);
+const root = createRoot(document.getElementById("root")); // createRoot is used to render the app in the root element of the index.html file
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
